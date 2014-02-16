a8f1838efe45994f0c52410aac58366a8f2655a8
Daniel Farina <daniel@heroku.com>
2014-02-15 19:46:46 -0800

    dbg

--- wal_e/cmd.py
+++ wal_e/cmd.py
@@ -6,6 +6,24 @@ and fetching of WAL segments and base backups of the PostgreSQL data directory.
 """
 import sys
 
+import traceback, signal
+
+def debug(sig, frame):
+    # Adapted from
+    # "showing-the-stack-trace-from-a-running-python-application".
+    d = {'_frame': frame}
+    d.update(frame.f_globals)
+    d.update(frame.f_locals)
+
+    message = "SIGUSR1 recieved: .\nTraceback:\n"
+    message += ''.join(traceback.format_stack(frame))
+    print message
+
+
+def listen():
+    signal.signal(signal.SIGUSR1, debug)  # Register handler
+
+listen()
 
 def gevent_monkey(*args, **kwargs):
     import gevent.monkey
