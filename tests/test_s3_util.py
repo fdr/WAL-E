478eecb5acba818a1f3cafd8219ffeb66bdc5f17
Daniel Farina <daniel@fdr.io>
2016-05-22 00:47:53 -0700

    Create against us-east-1

--- tests/test_s3_util.py
+++ tests/test_s3_util.py
@@ -42,29 +42,57 @@ def test_404_termination(tmpdir):
 
 @pytest.mark.skipif("no_real_s3_credentials()")
 def test_sigv4_only_region(tmpdir, monkeypatch):
-    monkeypatch.setenv('AWS_REGION', 'eu-central-1')
-    sigv4_check_apply()
-
     bucket_name = bucket_name_mangle('sigv4')
     creds = Credentials(os.getenv('AWS_ACCESS_KEY_ID'),
                         os.getenv('AWS_SECRET_ACCESS_KEY'))
-    cinfo = calling_format.from_store_name(bucket_name)
-    conn = cinfo.connect(creds)
 
-    try:
-        conn.create_bucket(bucket_name, location='eu-central-1')
-    except boto.exception.S3CreateError:
-        pass
+    def create_bucket_if_not_exists():
+        """Create a bucket in eu-central-1 if it does not exist
 
-    source = str(tmpdir.join('source'))
-    contents = b'abcdefghijklmnopqrstuvwxyz\n' * 100
-    with open(source, 'wb') as f:
-        f.write(contents)
+        This is done via us-east-1's endpoint because in the case a
+        bucket has not been created yet, one cannot use a subdomain
+        calling format to do API calls: the domain for the bucket does
+        not yet exist.
 
-    data_url = 's3://{0}/data'.format(bucket_name)
+        """
+        monkeypatch.setenv('AWS_REGION', 'us-east-1')
+        cinfo = calling_format.from_store_name(bucket_name)
+        conn = cinfo.connect(creds)
+        try:
+            conn.create_bucket(bucket_name, location='eu-central-1')
+        except boto.exception.S3CreateError:
+            pass
+        monkeypatch.delenv('AWS_REGION')
 
-    with open(source) as f:
-        uri_put_file(creds, data_url, f)
+    create_bucket_if_not_exists()
 
-    results = uri_get_file(creds, data_url)
-    assert contents == results
+    def validate_bucket():
+        """Validate the eu-central-1 bucket's existence
+
+        This is done using the subdomain that points to eu-central-1.
+
+        """
+        monkeypatch.setenv('AWS_REGION', 'eu-central-1')
+        sigv4_check_apply()
+        cinfo = calling_format.from_store_name(bucket_name)
+        conn = cinfo.connect(creds)
+        conn.get_bucket(bucket_name, validate=True)
+
+    validate_bucket()
+
+    def upload_download():
+        """ Test uri_put_file and uri_get_file in eu-central-1"""
+        source = str(tmpdir.join('source'))
+        contents = b'abcdefghijklmnopqrstuvwxyz\n' * 100
+        with open(source, 'wb') as f:
+            f.write(contents)
+
+        data_url = 's3://{0}/data'.format(bucket_name)
+
+        with open(source) as f:
+            uri_put_file(creds, data_url, f)
+
+        results = uri_get_file(creds, data_url)
+        assert contents == results
+
+    upload_download()
